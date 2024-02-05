2a267577a7faeb29eeaa15fd04a6a17b97ad21f3
Cameron Diaz <cdiazit5@gmail.com>
2024-02-05 16:35:51 -0500

    adding dashboard cards

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,7 +1,20 @@
+import { MdSupervisedUserCircle } from 'react-icons/md'
 import styles from './card.module.css'
 
 const Card = () => {
-    return <div className={styles.container}>Card</div>
+    return (
+        <div className={styles.container}>
+            <MdSupervisedUserCircle size={24} />
+            <div className={styles.texts}>
+                <span className={styles.title}>Total Users</span>
+                <span className={styles.number}>10.272</span>
+                <span className={styles.detail}>
+                    <span className={styles.positive}>12%</span>
+                    more than previous.
+                </span>
+            </div>
+        </div>
+    )
 }
 
 export default Card
