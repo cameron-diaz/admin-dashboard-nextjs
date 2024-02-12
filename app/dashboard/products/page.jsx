6ace26729cd7721ad51350c3201208128b9d3766
Cameron Diaz <cdiazit5@gmail.com>
2024-02-12 14:02:55 -0500

    product image

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import Pagination from '@/app/ui/dashboard/pagination/pagination'
+import Search from '@/app/ui/dashboard/search/search'
+import styles from '@/app/ui/dashboard/products/products.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const ProductsPage = () => {
-    return <div>product page</div>
+    return (
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Search for a Product..." />
+                <Link href="/dashboards/products/add">
+                    <button className={styles.addButton}>Add New</button>
+                </Link>
+            </div>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>Title</td>
+                        <td>Description</td>
+                        <td>Price</td>
+                        <td>Created At</td>
+                        <td>Stock</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.products}>
+                                <Image
+                                    src="/noproduct.jpg"
+                                    width={40}
+                                    height={40}
+                                    className={styles.productImage}
+                                />
+                                IPhone
+                            </div>
+                        </td>
+                        <td>Desc</td>
+                        <td>$999</td>
+                        <td>10/13/2023</td>
+                        <td>72</td>
+                        <td>
+                            <div className={styles.buttons}>
+                                <Link href="/">
+                                    <button
+                                        className={`${styles.button} ${styles.view}`}
+                                    >
+                                        View
+                                    </button>
+                                </Link>
+                                <Link href="/">
+                                    <button
+                                        className={`${styles.button} ${styles.delete}`}
+                                    >
+                                        Delete
+                                    </button>
+                                </Link>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <Pagination />
+        </div>
+    )
 }
 
 export default ProductsPage
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,6 +1,69 @@
+import Pagination from '@/app/ui/dashboard/pagination/pagination'
+import Search from '@/app/ui/dashboard/search/search'
 import styles from '@/app/ui/dashboard/users/users.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 const UsersPage = () => {
-    return <div className={styles.container}>users page</div>
+    return (
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Search for a User..." />
+                <Link href="/dashboards/users/add">
+                    <button className={styles.addButton}>Add New</button>
+                </Link>
+            </div>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Created At</td>
+                        <td>Role</td>
+                        <td>Status</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image
+                                    src="/noavatar.png"
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                John Doe
+                            </div>
+                        </td>
+                        <td>john.doe@gmail.com</td>
+                        <td>12.04.23</td>
+                        <td>Admin</td>
+                        <td>active</td>
+                        <td>
+                            <div className={styles.buttons}>
+                                <Link href="/">
+                                    <button
+                                        className={`${styles.button} ${styles.view}`}
+                                    >
+                                        View
+                                    </button>
+                                </Link>
+                                <Link href="/">
+                                    <button
+                                        className={`${styles.button} ${styles.delete}`}
+                                    >
+                                        Delete
+                                    </button>
+                                </Link>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <Pagination />
+        </div>
+    )
 }
 
 export default UsersPage
